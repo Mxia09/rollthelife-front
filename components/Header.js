8681e306450d7aece64b4089507facc008bb447f
Marvin Xia <marvinxia09@gmail.com>
2024-03-12 18:43:15 -0700

    nav bar and globalstyles

---
+++ components/Header.js
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import styled from "styled-components";
+import Center from "./Center";
+
+const StyledHeader = styled.header`
+  background-color: #222;
+  `;
+
+const Logo = styled(Link)`
+    color:#fff;
+    text-decoration:none;
+    `; 
+
+const Warpper = styled.div`
+display: flex;
+justify-content: space-between;
+padding: 20px 0;
+`;
+
+const StyledNav = styled.nav`
+    display:flex;
+    gap: 15px;
+`;
+
+const NavLink = styled(Link)`
+    color:#aaa;
+    text-decoration:none;
+`;
+
+export default function Header() {
+    return( 
+        <StyledHeader>
+            <Center>
+                <Warpper>
+                <Logo href={'/'}>
+                    Roll The Life 
+                </Logo>
+                <StyledNav>
+                    <NavLink href={'/'}>Home</NavLink>
+                    <NavLink href={'/products'}>Products</NavLink>
+                    <NavLink href={'/categories'}>Categories</NavLink>
+                    <NavLink href={'/account'}>Account</NavLink>
+                    <NavLink href={'/cart'}>Cart (0)</NavLink>
+                </StyledNav>
+                </Warpper>
+            </Center>
+        </StyledHeader>
+    )
+}
