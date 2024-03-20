47c93215538d1f1a3a7b54ee7b5aa695b0a16f70
Marvin Xia <marvinxia09@gmail.com>
2024-03-20 16:59:37 -0700

    cart item logic

--- components/CartContext.js
+++ components/CartContext.js
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const { createContext } = require("react");
 
 export const CartContext =createContext({});
 
 export function CartContextProvider({children}) {
+    const ls = typeof window !== "undefined" ? window.localStorage : null;
     const [cartProducts,setCartProducts] = useState([]);
+    useEffect(() => {
+        if (cartProducts?.length > 0) {
+            ls?.setItem('cart', JSON.stringify(cartProducts))
+        }
+    }, [cartProducts])
+    useEffect(() => {
+        if (ls && ls.getItem('cart')) {
+            setCartProducts(JSON.parse(ls.getItem('cart')));
+        }
+    }, []);
     function addProduct(ProductId) {
         setCartProducts(prev => [...prev, ProductId]);
     }
