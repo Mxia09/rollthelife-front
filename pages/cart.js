182a787e6f37292791c2c1a844d1472a0e87143c
Marvin Xia <marvinxia09@gmail.com>
2024-03-21 16:58:37 -0700

    cart data

--- pages/cart.js
+++ pages/cart.js
@@ -2,8 +2,9 @@ import Button from "@/components/Button";
 import { CartContext } from "@/components/CartContext";
 import Center from "@/components/Center";
 import Header from "@/components/Header";
-import { useContext } from "react";
+import { useContext, useEffect, useState} from "react";
 import styled, {css} from "styled-components";
+import axios from "axios";
 
 const ColumnsWrapper = styled.div`
     display: grid;
@@ -20,22 +21,47 @@ const Box = styled.div`
 
 export default function CartPage() {
     const {cartProducts} = useContext(CartContext);
+    const [products,setProducts] = useState([])
+
+    useEffect(() => {
+        if (cartProducts.length > 0) {
+            axios.post('/api/cart', {ids:cartProducts})
+                .then(response => {
+                    setProducts(response.data);
+                })
+        }
+    }, [cartProducts]);
     return(
         <>
             <Header/>
             <Center>
                 <ColumnsWrapper>
                     <Box>
+                        <h2>Cart</h2>
                         {!cartProducts?.length && (
                             <div>Your cart is empty</div>
                         )}
-                        {cartProducts?.length > 0 && (
-                            <>
-                            <h2>Cart</h2>
-                            {cartProducts.map(productId => (
-                                <div>{productId}</div>
-                            ))}
-                            </>
+                        {products?.length > 0 && (
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Product</th>
+                                        <th>Quantity</th>
+                                        <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {products.map(product => (
+                                        <tr>
+                                            <td>{product.title}</td>
+                                            <td>
+                                                {cartProducts.filter(id => id === product._id).length}
+                                            </td>
+                                            <td>Price</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         )}
                     </Box>
                     {!!cartProducts?.length && (
